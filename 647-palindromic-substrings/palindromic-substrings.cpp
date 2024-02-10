6120263b61a2547d5fac6568086ff742ed3be412
KunalParkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-02-10 21:13:05 +0530

    Time: 11 ms (41.17%) | Memory: 9.5 MB (56.37%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(isPalindrome(s,i,j)){
-                    count++;
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        // Base Case: single-character substrings
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // Initialize count with the number of single-character substrings
+        int count = n;
+
+        // Expand palindromes
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    if (len == 2 || dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        count++;
+                    }
                 }
             }
         }
-        return count;
-    }
 
-private:
-    bool isPalindrome(string& s,int start,int end){
-        while(start<end){
-            if(s[start]!=s[end]){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+        return count;
     }
 };
