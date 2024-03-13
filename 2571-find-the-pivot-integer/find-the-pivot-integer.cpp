a75df6bcba558d799b556ba89d584be993b04d17
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-03-13 21:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        for(int i=1;i<=n;i++){
-            int sumleft=0,sumright=0;
+        const int sum=(n*(n+1)/2);
+        const int pivot=sqrt(sum);
 
-            for(int j=1;j<=i;j++){
-                sumleft+=j;
-            }
-            for(int k=i;k<=n;k++){
-                sumright+=k;
-            }
-
-            if(sumleft==sumright){
-                return i;
-            }
-        }
-
-
-        return -1;
+        return pivot*pivot == sum?pivot:-1;
     }
 };
