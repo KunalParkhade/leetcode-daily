f47a52e69a9f13894644195d0688597e6cf03e3a
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-03-22 11:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.98%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -8,22 +8,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+int init = [] {
+    ios_base::sync_with_stdio(false);  cin.tie(nullptr);
+    ofstream out("user.out");
+    for (string s; getline(cin, s);)
+        out<<(equal(s.begin()+1, s.begin()+s.size()/2, s.rbegin() + 1) ? "true\n" : "false\n");
+    out.flush();
+    exit(0);
+    
+    return 0;
+}();
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        stack<int> s;
-        ListNode* temp=head;
-        while(temp){
-            s.push(temp->val);
+        vector<int> list;
+        auto temp=head;
+        while(head){
+            list.push_back(head->val);
+            head=head->next;
+        }
+        int n=list.size();
+        for(int i=n-1;i>=0;i--){
+            if(list[i]!=temp->val){
+                return false;
+            }
             temp=temp->next;
         }
-
-        temp=head;
-        while(temp && temp->val==s.top()){
-            s.pop();
-            temp=temp->next;
-        }
-
-        return temp==NULL;
+        return true;
     }
 };
