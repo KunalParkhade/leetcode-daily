95e46ffc22daa674793bf01cc51d03a421ca6d30
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-04-01 06:59:12 +0530

    Time: 4 ms (47.87%) | Memory: 7.6 MB (87.89%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n=s.size();
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]==' '){
-                if(ans!=0){
-                    break;
-                }
-            }
-            else{
-                ans++;
+        int length = 0;
+        bool foundWord = false;
+        
+        // Traverse the string from right to left
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                foundWord = true; // Mark that we have found a non-space character
+                length++; // Increment the length of the word
+            } else if (foundWord) {
+                // If we encounter a space after finding a word, then we have found the last word
+                break;
             }
         }
-        return ans;
+        
+        return length;
     }
 };
