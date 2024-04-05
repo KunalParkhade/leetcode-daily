8d7ea8da00bf425b638e7c76ad5d9eeafcd5c927
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-04-05 14:30:35 +0530

    Time: 6 ms (8.39%) | Memory: 8.8 MB (11.17%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        
+        for(char ch:s){
+            if(!st.empty()&& abs(st.top()-ch)==32){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+
+        string result;
+        while(!st.empty()){
+            result=st.top()+result;
+            st.pop();
+        }
+
+        return result;
+    }
+};
