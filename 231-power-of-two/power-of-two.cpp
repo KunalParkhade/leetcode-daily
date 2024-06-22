51f0f394b417ca9027d9d995509182008692087c
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-06-22 08:13:22 +0530

    Time: 5 ms (20.49%) | Memory: 7.2 MB (76.97%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-
-        if(n==0)    //edge case
+        if(n==0)
             return false;
-        
+
+        int ans=1;
+
         for(int i=0;i<=30;i++){
-            if(n==pow(2,i)){
+            if(ans==n){
                 return true;
             }
+            if(ans<INT_MAX/2)
+            ans=ans*2;
         }
         return false;
     }
