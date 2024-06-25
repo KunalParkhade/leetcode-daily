da88d5983ec4ec34beeac95ef59ddbd9ae16cf63
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-06-25 22:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (12.64%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        // Store the inorder traversal in an array.
+        vector<int> inorderTraversal;
+        inorder(root, inorderTraversal);
+
+        // Reverse the array to get descending order.
+        reverse(inorderTraversal.begin(), inorderTraversal.end());
+
+        // Modify the values in the tree.
+        replaceValues(root, inorderTraversal);
+
+        return root;
+    }
+
+private:
+    // Perform any traversal of your choice to store node values.
+    void inorder(TreeNode* root, vector<int>& inorderTraversal) {
+        if (root == nullptr) {
+            return;
+        }
+        inorder(root->left, inorderTraversal);
+        inorderTraversal.push_back(root->val);
+        inorder(root->right, inorderTraversal);
+    }
+    // Function to modify the values in the tree.
+    void replaceValues(TreeNode* root, vector<int>& inorderTraversal) {
+        if (root == nullptr) {
+            return;
+        }
+        replaceValues(root->left, inorderTraversal);
+        replaceValues(root->right, inorderTraversal);
+
+        int nodeSum = 0;
+        // Replace node with values greater than the current value.
+        for (auto i : inorderTraversal) {
+            if (i > root->val) {
+                nodeSum += i;
+            } else {
+                break;
+            }
+        }
+
+        root->val += nodeSum;
+    }
+};
