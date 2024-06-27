8c145506483eddfc9ea7b5fcaffc5f96990a9f70
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-06-27 08:35:57 +0530

    Time: 139 ms (53.43%) | Memory: 70.8 MB (54.65%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -4,11 +4,6 @@ public:
         vector<int> fEdge = edges[0];
         vector<int> sEdge = edges[1];
 
-        if(fEdge[0] == sEdge[0] || fEdge[0] == sEdge[1]){
-            return fEdge[0];
-        }
-        else{
-            return fEdge[1];
-        }
+        return (fEdge[0] == sEdge[0] || fEdge[0] == sEdge[1]) ? (fEdge[0]) : (fEdge[1]);
     }
 };
