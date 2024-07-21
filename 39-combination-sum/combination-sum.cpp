982027b86d2ae77497dee2826623ca963a46e807
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-07-21 20:45:06 +0530

    Time: 4 ms (72.41%) | Memory: 12.4 MB (69.35%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void findCombinationHelper(int ind, vector<int>& arr, int target,
+                               vector<int>& ds, vector<vector<int>>& ans) {
+        if (ind == arr.size()) {
+            if (target == 0) {
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        if (arr[ind] <= target) {
+            // Condition for Pick up the element
+            ds.push_back(arr[ind]);
+            findCombinationHelper(ind, arr, target - arr[ind], ds, ans);
+            ds.pop_back();
+        }
+
+        // Not Pick
+        findCombinationHelper(ind + 1, arr, target, ds, ans);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        findCombinationHelper(0, candidates, target, ds, ans);
+
+        return ans;
+    }
+};
