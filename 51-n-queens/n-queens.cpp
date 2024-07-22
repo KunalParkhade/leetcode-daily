6484cdf19fa2113e5b431c00973e0ef92f02f755
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-07-22 18:50:42 +0530

    Time: 2 ms (83.51%) | Memory: 9.5 MB (63.59%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,51 +1,68 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string> board, int n) {
-        int duprow = row;
-        int dupcol = col;
+    // bool isSafe(int row, int col, vector<string> board, int n) {
+    //     int duprow = row;
+    //     int dupcol = col;
 
-        // check upper diagonal
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row--;
-            col--;
-        }
+    //     // check upper diagonal
+    //     while (row >= 0 && col >= 0) {
+    //         if (board[row][col] == 'Q')
+    //             return false;
+    //         row--;
+    //         col--;
+    //     }
 
-        // check on left
-        row = duprow;
-        col = dupcol;
-        while (col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            col--;
-        }
+    //     // check on left
+    //     row = duprow;
+    //     col = dupcol;
+    //     while (col >= 0) {
+    //         if (board[row][col] == 'Q')
+    //             return false;
+    //         col--;
+    //     }
 
-        // check on lower diagonal
-        row = duprow;
-        col = dupcol;
-        while (row < n && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row++;
-            col--;
-        }
+    //     // check on lower diagonal
+    //     row = duprow;
+    //     col = dupcol;
+    //     while (row < n && col >= 0) {
+    //         if (board[row][col] == 'Q')
+    //             return false;
+    //         row++;
+    //         col--;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
     void solve(int col, vector<string>& board, vector<vector<string>>& ans,
-               int n) {
+               vector<int>& leftRow, vector<int>& upperDiagonal,
+               vector<int>& lowerDiagonal, int n) {
         if (col == n) {
             ans.push_back(board);
             return;
         }
 
+        // for (int row = 0; row < n; row++) {
+        //     if (isSafe(row, col, board, n)) {
+        //         board[row][col] = 'Q';
+        //         solve(col + 1, board, ans, n);
+        //         board[row][col] = '.';
+        //     }
+        // }
         for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, board, n)) {
+            // checking the queen is safe or not
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 &&
+                upperDiagonal[n - 1 + col - row] == 0) {
                 board[row][col] = 'Q';
-                solve(col + 1, board, ans, n);
-                board[row][col] = '.';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
+                solve(col + 1, board, ans, leftRow, upperDiagonal,
+                      lowerDiagonal, n);
+                board[row][col]='.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
             }
         }
     }
@@ -59,7 +76,10 @@ public:
             board[i] = s;
         }
 
-        solve(0, board, ans, n);
+        vector<int> leftRow(n, 0), upperDiagonal(2 * n - 1, 0),
+            lowerDiagonal(2 * n - 1, 0);
+
+        solve(0, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
 
         return ans;
     }
