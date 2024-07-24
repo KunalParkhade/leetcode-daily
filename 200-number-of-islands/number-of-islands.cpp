216aa8809c4caef5d43ad0d847cf607ae2210097
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-07-24 19:14:17 +0530

    Time: 23 ms (90.10%) | Memory: 24.1 MB (15.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int n, m;
-    bool inside(int i, int j) {
-        return (0<=i)&&(i<n)&&(0<=j)&&(j<m);
-    }
-    void dfs(int i, int j, vector<vector<char>>& grid){
-        if (!inside(i, j)) return;
-        if (grid[i][j]=='1'){
-            grid[i][j]='2';
-            dfs(i+1,j, grid);
-            dfs(i,j+1, grid);
-            dfs(i-1,j, grid);
-            dfs(i,j-1, grid);
-        }           
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        n=grid.size();
-        m=grid[0].size();
-        int num=0;
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++){
-                if(grid[i][j]=='1'){
-                    num++;
-                    dfs(i, j, grid);   
+    void bfs(int row, int col, vector<vector<int>>& visited, vector<vector<char>>& grid) {
+        visited[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        int n = grid.size();
+        int m = grid[0].size();
+
+        // These arrays are used to get row and column numbers of 4 neighbors of a given cell
+        int dRow[] = {-1, 0, 1, 0};
+        int dCol[] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int currRow = q.front().first;
+            int currCol = q.front().second;
+            q.pop();
+
+            // Traverse the 4 neighbors (up, down, left, right)
+            for (int i = 0; i < 4; i++) {
+                int newRow = currRow + dRow[i];
+                int newCol = currCol + dCol[i];
+
+                // Check if the new cell is within bounds and is a land cell and not visited
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && 
+                    grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
+                    visited[newRow][newCol] = 1;
+                    q.push({newRow, newCol});
                 }
             }
-        return num;
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        int cnt = 0;
+        
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (!visited[row][col] && grid[row][col] == '1') {
+                    cnt++;
+                    bfs(row, col, visited, grid);
+                }
+            }
+        }
+        return cnt;
     }
 };
