ad8b2858607d49b683f36bef6a375233b25bb393
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-09-07 20:58:37 +0530

    Time: 79 ms (86.16%) | Memory: 99 MB (41.43%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,7 +4,7 @@ public:
                 vector<vector<int>>& dp) {
         if (target == 0)
             return true;
-            
+
         if (ind == 0)
             return (arr[0] == target);
         if (dp[ind][target] != -1)
@@ -31,9 +31,27 @@ public:
 
         int totSum = sum / 2;
 
-        vector<vector<int>> dp(n, vector<int>(totSum + 1, -1));
+        vector<vector<int>> dp(n, vector<int>(totSum + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if (nums[0] <= totSum) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int target = 1; target <= totSum; target++) {
+                bool notTake = dp[i - 1][target];
+                bool take = false;
+                if (target >= nums[i])
+                    take = dp[i - 1][target - nums[i]];
+                dp[i][target] = take || notTake;
+            }
+        }
+
+        return dp[n - 1][totSum];
 
         // return Helper(n - 1, nums, sum/2);
-        return Helper(n-1, totSum, nums, dp);
+        // return Helper(n-1, totSum, nums, dp);
     }
 };
