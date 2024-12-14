224eb402d69f61d57a3bbba4059aec34d9df10db
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-12-14 21:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (77.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,11 +6,11 @@ public:
         vector<int> hash(255, -1);
 
         while(r<n){
-            if(hash[s[r]]!=-1){
+            // if(hash[s[r]]!=-1){
                 if(hash[s[r]]>=l){
                     l = hash[s[r]] + 1;
                 }
-            }
+            // }
             maxlen = max(maxlen, r-l+1);
             hash[s[r]] = r;
             r++;
