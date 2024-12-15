559c8e96d05572038f7f22c6101eb2566f8f77e4
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-12-15 21:15:45 +0530

    Time: 3 ms (73.84%) | Memory: 15.2 MB (5.41%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // If the array length is less than or equal to 2, return its length directly.
-        if (nums.size() <= 2) return nums.size();
+        unordered_map<int, int> count;
+        int k = 0;
 
-        // Initialize the write index at 1 since the first element is always allowed.
-        int write_index = 1;
-
-        // Initialize a counter for the current element occurrence.
-        int count = 1;
-
-        // Iterate from the second element to the end of the array.
-        for (int read_index = 1; read_index < nums.size(); read_index++) {
-            if (nums[read_index] == nums[read_index - 1]) {
-                // If the current element is the same as the previous one, increment the count.
-                count++;
-            } else {
-                // If the current element is different, reset the count to 1.
-                count = 1;
-            }
-
-            // If the count is less than or equal to 2, copy the element to write_index and increment write_index.
-            if (count <= 2) {
-                nums[write_index] = nums[read_index];
-                write_index++;
+        for(int num:nums){
+            count[num]++;
+            if(count[num]<=2){
+                nums[k] = num;
+                k++;
             }
         }
-
-        // Return the new length of the array where duplicates appear at most twice.
-        return write_index;
+        return k;
     }
 };
