7b332aa2a5d741221de1eb24912608bb3f052a78
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-12-31 11:53:19 +0530

    Time: 14 ms (37.09%) | Memory: 19 MB (8.04%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,45 +9,36 @@
  */
 class Solution {
 public:
-    // bool findpath(TreeNode* root, vector<TreeNode *> &path, TreeNode* data){
-    //     if(root == nullptr) return false;
+    bool findpath(TreeNode* root, vector<TreeNode*>& path, TreeNode* temp) {
+        if (!root)
+            return false;
 
-    //     path.push_back(root);
+        path.push_back(root);
 
-    //     if(root->val == data->val) return true;
+        if (root->val == temp->val)
+            return true;
 
-    //     if(findpath(root->left, path, data)||findpath(root->right, path, data)) return true;
+        if (findpath(root->left, path, temp) ||
+            findpath(root->right, path, temp)) {
+            return true;
+        }
 
-    //     path.pop_back();
-
-    //     return false;
-    // }
+        path.pop_back();
+        return false;
+    }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        //Optimized solution (Single Traversal)
+        vector<TreeNode*> p1, p2;
 
-        if(root == nullptr || root == p || root == q)   return root;
-
-        TreeNode* left_lca = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right_lca = lowestCommonAncestor(root->right, p, q);
-
-        if(left_lca!=nullptr && right_lca!=nullptr)   return root;
-
-        return (left_lca != nullptr) ? left_lca : right_lca;
-
-        //Not Optimized
-        // vector<TreeNode *> p1, p2;
-
-        // if(!findpath(root, p1, p) || !findpath(root, p2, q)) return nullptr;
-
-        // int i;
-
-        // for(i = 0;i<p1.size() && i<p2.size();i++){
-        //     if(p1[i]!=p2[i]){
-        //         break;
-        //     }
-        // }
-
-        // return p1[i-1];
+        if (!findpath(root, p1, p) || !findpath(root, p2, q)) {
+            return nullptr;
+        }
+        int i;
+        for (i = 0; i < p1.size() && i < p2.size(); i++) {
+            if (p1[i] != p2[i]) {
+                break;
+            }
+        }
+        return p1[i-1];
     }
 };
