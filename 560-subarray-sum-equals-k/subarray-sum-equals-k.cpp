2eb44b2e8ed7fe3ef3c1618a2977938acf88c440
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2025-02-14 21:55:54 +0530

    Time: 41 ms (72.98%) | Memory: 45.4 MB (67.88%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        int count = 0;
-        
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                }
-            }
-        }
+        int count = 0, sum = 0;
+        unordered_map<int, int> prefixSumCount;
+        prefixSumCount[0] = 1; // Initialize to handle cases where sum == k directly
 
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+//             For each element, it checks if there exists a previous prefix sum such that when     subtracted from the current cumulative sum (sum), it equals 
+// k
+// k. This is done by checking if sum - k exists in prefixSumCount.
+            // Check if there exists a prefix sum that makes the subarray sum to k
+            if (prefixSumCount.find(sum - k) != prefixSumCount.end()) {
+                count += prefixSumCount[sum - k];
+            }
+
+            // Increment the count of the current prefix sum
+            prefixSumCount[sum]++;
+        }
         return count;
     }
 };
